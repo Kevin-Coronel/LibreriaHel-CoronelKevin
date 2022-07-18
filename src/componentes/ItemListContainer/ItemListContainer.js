29ca970be005906a8b35086b0065ca5de49c10e2
Kevin Coronel <kevin.coronel227@gmail.com>
2022-07-18 19:56:11 -0300

    Entrega Desafio Clase 2 y 3

--- src/componentes/ItemListContainer/ItemListContainer.js
+++ src/componentes/ItemListContainer/ItemListContainer.js
@@ -3,7 +3,7 @@ import './ItemListContainer.scss';
 const ItemListContainer = () =>{
     return(
         <div>
-            <h1>Greetings</h1>
+            <h1 class="greet">Greetings</h1>
         </div>
 
     )
